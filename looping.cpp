3fe75641d7bfdb7482d6e747f5266dbb81b0bd69
Martzz-a <maritzafidelyah@gmail.com>
2026-03-30 16:23:15 +0700

    Menambahkan For Loop

--- looping.cpp
+++ looping.cpp
@@ -6,4 +6,13 @@ int main (){
     int x;
 
     cout << "PERULANGAN FOR" << endl;
+
+    // Perulangan dengan FOR
+    // mencetak tulisan sebanyak 5 kali
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Teknologi Informasi UMY " << i << endl;
+    }
+    
+
 }
